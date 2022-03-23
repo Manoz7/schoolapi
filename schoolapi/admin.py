537f9ae7019f1cf00916abb2f3dcb2263839adf4
manoz7 <manojkhanal936@gmail.com>
2022-03-23 13:43:02 +0545

    Models Added and Changes

--- schoolapi/admin.py
+++ schoolapi/admin.py
@@ -1,7 +1,6 @@
-from ast import Sub
 from django.contrib import admin
-from schoolapi.models import Teacher, Subject
+from schoolapi.models import Teacher, Subject, Event, Notice
 # Register your models here.
 
 
-admin.site.register((Teacher, Subject))
+admin.site.register((Teacher, Subject, Event, Notice))
--- schoolapi/models.py
+++ schoolapi/models.py
@@ -1,10 +1,16 @@
-
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+import os
 
-# Create your models here.
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
-class Subject(models.Model):
+    class Meta:
+        abstract = True
+        
+class Subject(TimeStamp):
     subject_id = models.AutoField(primary_key=True, null=False)
     subject_name = models.CharField(max_length=100)
 
@@ -15,7 +21,7 @@ class Subject(models.Model):
     def __str__(self) -> str:
         return self.subject_name
 
-class Teacher(models.Model):
+class Teacher(TimeStamp):
     full_name = models.CharField(max_length=100)
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200)
@@ -34,3 +40,46 @@ class Teacher(models.Model):
 
     def __str__(self) -> str:
         return self.full_name
+
+# To change the name of images with the title of the instance of the class
+def name_of_image(instance, filename):
+    if hasattr(instance, 'imgpath'):
+        if os.path.exists(instance.imgpath):
+            os.remove(instance.imgpath)
+            
+    blocks = filename.split('.')
+    ext = blocks[-1]
+    filename = "%s.%s" % (instance.title, ext)
+    instance.title = blocks[0]
+    instance.imgpath = os.path.join('manaslu/notice/', filename)
+    return instance.imgpath
+    
+class Notice(TimeStamp):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    photo = models.ImageField(upload_to=name_of_image, blank=True, null=True)
+    notice_date = models.DateField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = _('Notice')
+        verbose_name_plural = _('Notices')
+
+    def __str__(self) -> str:
+        return self.title
+    
+   
+class Event(TimeStamp):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    organizer = models.CharField(max_length=100, null=True, blank=True)
+    participants = models.CharField(max_length=100, null=True, blank=True)
+    published_date = models.DateField()
+    no_of_days = models.IntegerField(blank=True, default=1)
+    event_date = models.DateField()
+
+    class Meta:
+        verbose_name = _('Event')
+        verbose_name_plural = _('Events')
+
+    def __str__(self) -> str:
+        return self.title
